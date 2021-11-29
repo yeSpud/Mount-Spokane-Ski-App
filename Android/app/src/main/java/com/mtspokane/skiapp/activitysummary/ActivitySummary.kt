b5deb94d43fce41368546c6642dc8c229c2752a5
Spud <jeffrypig23@gmail.com>
2021-11-29 11:29:41 -0800

    Moved gitnignore file

--- Android/app/src/main/java/com/mtspokane/skiapp/activitysummary/ActivitySummary.kt
+++ Android/app/src/main/java/com/mtspokane/skiapp/activitysummary/ActivitySummary.kt
@@ -2,13 +2,19 @@ package com.mtspokane.skiapp.activitysummary
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TableRow
+import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.setPadding
+import com.mtspokane.skiapp.R
 import com.mtspokane.skiapp.databinding.ActivitySummaryBinding
 
 class ActivitySummary: Activity() {
 
-	lateinit var binding: ActivitySummaryBinding
+	private lateinit var binding: ActivitySummaryBinding
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -35,19 +41,63 @@ class ActivitySummary: Activity() {
 	private fun addActivity(activity: SkiingActivity) {
 
 		val view = createActivityView(activity)
-
 		this.binding.container.addView(view)
 	}
 
-	private fun createActivityView(activity: SkiingActivity): SkiingActivityLayout {
+	private fun createActivityView(activity: SkiingActivity): LinearLayout {
 
-		val view = SkiingActivityLayout(this, activity.name, activity.location)
+		val linearLayout = LinearLayout(this)
+		linearLayout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+		linearLayout.orientation = LinearLayout.HORIZONTAL
+		linearLayout.setPadding(10)
 
+
+		val imageView = ImageView(this)
+		val imageLayoutParams = this.createLayoutParameters(ViewGroup.MarginLayoutParams.MATCH_PARENT,
+			ViewGroup.MarginLayoutParams.WRAP_CONTENT, 5)
+		imageView.layoutParams = imageLayoutParams
+		imageView.contentDescription = this.getText(R.string.icon_description)
 		if (activity.icon != null) {
-			view.icon.setImageDrawable(AppCompatResources.getDrawable(this, activity.icon))
+			imageView.setImageDrawable(AppCompatResources.getDrawable(this, activity.icon))
 		}
+		linearLayout.addView(imageView)
 
-		return view
+
+		val nameView = TextView(this)
+		val nameLayoutParams = this.createLayoutParameters(ViewGroup.MarginLayoutParams.WRAP_CONTENT,
+			45, 5)
+		nameView.layoutParams = nameLayoutParams
+		nameView.maxWidth = 249
+		nameView.textSize = 20F
+		nameView.text = activity.name
+		linearLayout.addView(nameView)
+
+
+		val timeView = TextView(this)
+		val timeLayoutParams = this.createLayoutParameters(0, 45, 5)
+		val weightLayoutParams = TableRow.LayoutParams(timeLayoutParams)
+		weightLayoutParams.weight = 10F
+		timeView.layoutParams = weightLayoutParams
+		timeView.minWidth = 64
+		timeView.textSize = 12F
+		timeView.text = " - ${activity.location.time}" // TODO Convert this from unix epoch to actual time
+		linearLayout.addView(timeView)
+
+
+		val arrowTextView = TextView(this)
+		val arrowLayoutParams = this.createLayoutParameters(ViewGroup.MarginLayoutParams.WRAP_CONTENT,
+			45, 0)
+		arrowTextView.layoutParams = arrowLayoutParams
+		arrowTextView.text = this.getText(R.string.arrow)
+		arrowTextView.textSize = 35F
+		linearLayout.addView(arrowTextView)
+
+		return linearLayout
 	}
 
+	private fun createLayoutParameters(width: Int, height: Int, marginEnd: Int): ViewGroup.MarginLayoutParams {
+		val layoutParameter: ViewGroup.MarginLayoutParams = ViewGroup.MarginLayoutParams(width, height)
+		layoutParameter.marginEnd = marginEnd
+		return layoutParameter
+	}
 }
