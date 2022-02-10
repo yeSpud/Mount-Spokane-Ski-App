6b04c33fe1a28604c57ebb2bb322ccd3c960ad93
Spud <jeffrypig23@gmail.com>
2022-02-09 16:50:39 -0800

    Removed elevation database

--- Android/app/src/main/java/com/mtspokane/skiapp/databases/Elevation.kt
+++
@@ -1,6 +0,0 @@
-package com.mtspokane.skiapp.databases
-
-import androidx.room.Entity
-
-@Entity(tableName = "Elevation", primaryKeys = ["latitude", "longitude"])
-data class Elevation(val latitude: Double, val longitude: Double, val elevation: Double)
